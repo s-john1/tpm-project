8e0f3e04ebd5e0415d0760c6e1e1d6d41ec92a95
Samuel Johnston <S.Johnston1@newcastle.ac.uk>
2024-05-08 19:49:23 +0100

    Key generation

--- client.py
+++ client.py
@@ -1,8 +1,16 @@
 import socket
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
 
 class Client:
     _sock = None
+    _server_public_key = None
+
+    __private_key = None
+    _public_key = None
 
     def __init__(self, address, port):
         self._address = address
@@ -26,9 +34,47 @@ class Client:
 
         print(f"Finished sending file {filename}")
 
+    def receive_public_key(self):
+        # Receive the server's public encryption key
+        data = self._sock.recv(1024)
 
+        if not data:
+            print("Unable to receive public key from the server")
+            return
+
+        print(data.decode("utf-8"))
+
+        # Deserialise the public key
+        key = load_pem_public_key(data)
+        self._server_public_key = key
+
+    def generate_signing_keys(self):
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+
+        self.__private_key = private_key
+        self._public_key = public_key
+
+        print("Successfully generated RSA signing keys")
+
+    def tpm_load_key(self):
+        if not self.__private_key:
+            print("Can't load private key to TPM")
+            return
+
+        pem = self._public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        with open("private.pem", "wb") as keyfile:
+            keyfile.write(pem)
+
+        # TODO: delete local pem
+        # TODO: delete variables
 
 
 if __name__ == "__main__":
     client = Client("127.0.0.1", 8120)
-    client.send_file("text.txt")
+    client.receive_public_key()
+    #client.send_file("text.txt")
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import socket
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
 
 class Server:
     _sock = None
     _conn = None
 
+    __private_key = None
+    _public_key = None
+
     def __init__(self, listen_addr, port):
         self.listen_addr = listen_addr
         self.port = port
+
+        # Generate RSA encryption keys
+        self.generate_keys()
 
         # Create TCP socket
         self._create_listener()
@@ -36,7 +45,33 @@ class Server:
 
         print("Received file")
 
+    def generate_keys(self):
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+
+        self.__private_key = private_key
+        self._public_key = public_key
+
+        print("Successfully generated RSA encryption keys")
+
+    def send_public_key(self):
+        if not self.__private_key:
+            print("No public key exists")
+            return
+
+        # Serialise key to PEM format
+        key_pem = self._public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        self._conn[0].send(key_pem)
+
+        print("Sent public encryption key to client")
+
+
 
 if __name__ == '__main__':
     server = Server('127.0.0.1', 8120)
-    server.receive_file("output")
+    server.send_public_key()
+    #server.receive_file("output")
