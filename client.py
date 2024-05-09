1812734aa9b6c6598fe0682ee772b039996f24b9
Samuel Johnston <S.Johnston1@newcastle.ac.uk>
2024-05-09 13:45:58 +0100

    Load signing key to TPM

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
+import os
 import socket
+import subprocess
 
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -12,16 +14,27 @@ class Client:
     __private_key = None
     _public_key = None
 
+    _TSS_DIR = "utils"
+
     def __init__(self, address, port):
         self._address = address
         self._port = port
 
         self._connect()
+        self._setup_tpm()
 
     def _connect(self):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.connect((self._address, self._port))
         print(f"Connected to {self._address}:{self._port}")
+
+    def _setup_tpm(self):
+        print("Setting up TPM:")
+        subprocess.run("./pcrreset -ha 16", shell=True, cwd=self._TSS_DIR)
+        subprocess.run("./flushcontext -ha 80000000", shell=True, cwd=self._TSS_DIR)
+        subprocess.run("./flushcontext -ha 80000001", shell=True, cwd=self._TSS_DIR)
+        subprocess.run("./createprimary -hi o", shell=True, cwd=self._TSS_DIR)
+        print("TPM is setup")
 
     def send_file(self, filename):
         f = open(filename, 'rb')
@@ -42,8 +55,6 @@ class Client:
             print("Unable to receive public key from the server")
             return
 
-        print(data.decode("utf-8"))
-
         # Deserialise the public key
         key = load_pem_public_key(data)
         self._server_public_key = key
@@ -58,23 +69,51 @@ class Client:
         print("Successfully generated RSA signing keys")
 
     def tpm_load_key(self):
+        TEMP_DIR = "/tmp/tpm-client"
+
         if not self.__private_key:
             print("Can't load private key to TPM")
             return
 
-        pem = self._public_key.public_bytes(
+        pem = self.__private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
         )
 
-        with open("private.pem", "wb") as keyfile:
-            keyfile.write(pem)
+        # Create temporary working directory
+        os.makedirs(TEMP_DIR, exist_ok=True)
 
-        # TODO: delete local pem
-        # TODO: delete variables
+        # Export private key
+        with open(TEMP_DIR+"/private.pem", "wb") as keyfile:
+            keyfile.write(pem)
+        print("Exported public and private keys")
+
+        # Import the private key to the TPM
+        subprocess.run(f"./importpem -hp 80000000 -ipem {TEMP_DIR}/private.pem -rsa -si -opu {TEMP_DIR}/pub.bin -opr {TEMP_DIR}/priv.bin",
+                       shell=True, cwd=self._TSS_DIR)
+        subprocess.run(f"./load -hp 80000000 -ipu {TEMP_DIR}/pub.bin -ipr {TEMP_DIR}/priv.bin",
+                       shell=True, cwd=self._TSS_DIR)
+        print("TPM has imported the private key")
+
+        # Cleanup local files and variables
+        os.remove(TEMP_DIR+"/private.pem")
+        os.remove(TEMP_DIR+"/pub.bin")
+        os.remove(TEMP_DIR+"/priv.bin")
+        os.rmdir(TEMP_DIR)
+        del self.__private_key
+
+    def _sign_file(self, file):
+        # Use the TPM to sign the file
+        subprocess.run(f"./sign -hk 80000001 -if {file} -halg sha256 -os ../sig.bin",
+                       shell=True, cwd=self._TSS_DIR)
 
 
 if __name__ == "__main__":
     client = Client("127.0.0.1", 8120)
     client.receive_public_key()
+
+    client.generate_signing_keys()
+    client.tpm_load_key()
+
     #client.send_file("text.txt")
